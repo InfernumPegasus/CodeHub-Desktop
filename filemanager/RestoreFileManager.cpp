d71068c5a7ea452a069fb1dad8f7665ee121c5c5
Vladimir Kirillov <vkirillov505@gmail.com>
2023-04-19 13:16:46 +0300

    `RestoreFileManager` class added

---
+++ filemanager/RestoreFileManager.cpp
@@ -0,0 +1,43 @@
+#include <fstream>
+#include <iostream>
+#include "RestoreFileManager.h"
+
+namespace fs = std::filesystem;
+
+bool RestoreFileManager::CreateRecoveryFolder(
+        const fs::path &folder) {
+    return !fs::exists(folder) &&
+           fs::create_directory(folder);
+}
+
+void RestoreFileManager::CopyFiles(
+        const std::unordered_set<File> &files,
+        const fs::path &from,
+        const fs::path &to) {
+    try {
+        for (const auto &file: files) {
+            const auto targetParentPath =
+                    to / fs::relative(file.Name(), from).parent_path();
+            fs::create_directories(targetParentPath);
+
+            fs::copy(file.Name(),
+                     targetParentPath,
+                     fs::copy_options::overwrite_existing);
+        }
+    } catch (std::exception &e) {
+        std::cout << e.what() << "\n";
+    }
+}
+
+void RestoreFileManager::CopyRecursive(
+        const fs::path &src,
+        const fs::path &target) {
+    try {
+        fs::copy(src,
+                 target,
+                 fs::copy_options::overwrite_existing |
+                 fs::copy_options::recursive);
+    } catch (std::exception &e) {
+        std::cout << e.what();
+    }
+}
---
+++ filemanager/RestoreFileManager.h
@@ -0,0 +1,22 @@
+#ifndef CODEHUB_RESTOREFILEMANAGER_H
+#define CODEHUB_RESTOREFILEMANAGER_H
+
+#include "../commit/Commit.h"
+#include <filesystem>
+
+namespace fs = std::filesystem;
+
+class RestoreFileManager {
+public:
+    static bool CreateRecoveryFolder(const fs::path &folder);
+
+    static void CopyFiles(const std::unordered_set<File> &files,
+                          const fs::path &from,
+                          const fs::path &to);
+
+    static void CopyRecursive(const fs::path &src,
+                              const fs::path &target);
+};
+
+
+#endif //CODEHUB_RESTOREFILEMANAGER_H
