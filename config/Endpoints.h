a4468ea05a6e4f2a6fcacce19ecb2b95ba3c3e3d
Vladimir Kirillov <vkirillov505@gmail.com>
2023-05-04 01:30:58 +0300

    `std::string_view` -> `std::string`

--- config/Endpoints.h
+++ config/Endpoints.h
@@ -5,30 +5,30 @@
 
 /* USERS endpoints */
 
-constexpr std::string_view BASE_USERS_URL =
+const std::string BASE_USERS_URL =
         "http://localhost:8000/users/";
 
 /* COMMITS endpoints */
 
-constexpr std::string_view BASE_COMMITS_URL =
+const std::string BASE_COMMITS_URL =
         "http://localhost:8000/commits/";
 
 /* TOKENS endpoints*/
 
-constexpr std::string_view BASE_TOKENS_URL =
+const std::string BASE_TOKENS_URL =
         "http://localhost:8000/tokens/";
 
 /* FILES endpoints */
 
-constexpr std::string_view BASE_FILES_URL =
+const std::string BASE_FILES_URL =
         "http://localhost:8000/files/";
 
 /* REFRESH endpoints */
-constexpr std::string_view BASE_REFRESH_URL =
+const std::string BASE_REFRESH_URL =
         "http://localhost:8000/refresh/";
 
 /* REPOSITORIES endpoints */
-constexpr std::string_view BASE_REPOSITORIES_URL =
+const std::string BASE_REPOSITORIES_URL =
         "http://localhost:8000/repositories/";
 
 
