ad61ab5909f1a0e8024418fb5b41f84b8d27bd49
Vladimir Kirillov <vkirillov505@gmail.com>
2023-07-13 17:37:14 +0300

    `std::string`->`fs::path`, `clang-format` off

--- include/config/ConfigFiles.h
+++ include/config/ConfigFiles.h
@@ -1,18 +1,23 @@
 #ifndef CODEHUB_CONFIGFILES_H
 #define CODEHUB_CONFIGFILES_H
 
+#include <filesystem>
 #include <string>
 
-constexpr std::string CONFIG_DIRECTORY = ".codehub_config";
-constexpr std::string CONFIG_FILE = ".repo_info.json";
-constexpr std::string COMMITS_FILE = ".commits.json";
-constexpr std::string IGNORE_FILE = ".ignore";
+namespace fs = std::filesystem;
+
+// clang-format off
+constexpr std::string CONFIG_DIRECTORY      = ".codehub_config";
+constexpr std::string CONFIG_FILE           = ".repo_info.json";
+constexpr std::string COMMITS_FILE          = ".commits.json";
+constexpr std::string IGNORE_FILE           = ".ignore";
 
 constexpr std::string VCS_REPOSITORIES_FILE = "repos.json";
 
-constexpr std::string VCS_USER_FILE = "user.json";
+constexpr std::string VCS_USER_FILE         = "user.json";
 
-constexpr std::string COOKIES_FILE = ".cookies.json";
-const std::string CONFIG_COOKIES_FILE = CONFIG_DIRECTORY + "/" + COOKIES_FILE;
+constexpr std::string COOKIES_FILE          = ".cookies.json";
+const fs::path CONFIG_COOKIES_FILE          = fs::path{CONFIG_DIRECTORY} / COOKIES_FILE;
+// clang-format on
 
 #endif  // CODEHUB_CONFIGFILES_H
