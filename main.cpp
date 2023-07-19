9f37f701c430d14109e40809ac2c4d70575417e6
Vladimir Kirillov <vkirillov505@gmail.com>
2023-07-19 19:09:40 +0300

    anonymous namespace with options added

--- main.cpp
+++ main.cpp
@@ -6,6 +6,22 @@
 
 namespace po = boost::program_options;
 
+// clang-format off
+namespace {
+constexpr auto HELP         = "help";
+constexpr auto INIT         = "init";
+constexpr auto STATUS       = "status";
+constexpr auto DIFF         = "diff"; // TODO fix
+constexpr auto REPOSITORIES = "repositories";
+constexpr auto LOG          = "log";
+constexpr auto COMMIT       = "commit";
+constexpr auto BRANCH       = "branch"; // TODO implement
+constexpr auto CHECKOUT     = "checkout"; // TODO implement
+constexpr auto RESTORE      = "restore";
+constexpr auto PUSH         = "push";
+}
+// clang-format on
+
 int main(int argc, char* argv[]) {
   VersionControlSystem vcs;
   vcs.Init();
@@ -13,49 +29,49 @@ int main(int argc, char* argv[]) {
   po::options_description description("Allowed options");
   // clang-format off
   description.add_options()
-          ("help", "produce help message")
-          ("init", po::value<std::string>(), "init new repository")
-          ("status", "get repository status")
-          ("diff", po::value<std::string>(),
+          (HELP, "produce help message")
+          (INIT, po::value<std::string>(), "init new repository")
+          (STATUS, "get repository status")
+          (DIFF, po::value<std::string>(),
           "shows file difference with last committed one")
-          ("repositories", "show all repositories")
-          ("log", "show commits history")
-          ("commit", po::value<std::string>(), "store changes")
+          (REPOSITORIES, "show all repositories")
+          (LOG, "show commits history")
+          (COMMIT, po::value<std::string>(), "store changes")
 
           // TODO implement
-          ("branch", po::value<std::string>(),"create new branch")
+          (BRANCH, po::value<std::string>(),"create new branch")
 
           // TODO
-          ("checkout", po::value<std::vector<std::string>>(), "switch to another branch")
+          (CHECKOUT, po::value<std::vector<std::string>>(), "switch to another branch")
 
-          ("restore", po::value<size_t>(),
+          (RESTORE, po::value<size_t>(),
           "remove file from index or get it back to committed condition")
-          ("push", "update external links and objects");
+          (PUSH, "update external links and objects");
   // clang-format on
 
   try {
     po::variables_map vm;
     po::store(po::parse_command_line(argc, argv, description), vm);
-    if (vm.empty() || vm.contains("help")) {
+    if (vm.empty() || vm.contains(HELP)) {
       std::cout << description << std::endl;
-    } else if (vm.contains("init")) {
-      vcs.CreateRepository(vm["init"].as<std::string>());
-    } else if (vm.contains("status")) {
+    } else if (vm.contains(INIT)) {
+      vcs.CreateRepository(vm[INIT].as<std::string>());
+    } else if (vm.contains(STATUS)) {
       vcs.CheckStatus();
-    } else if (vm.contains("diff")) {
-      vcs.ShowFileDifference(vm["diff"].as<std::string>());
-    } else if (vm.contains("repositories")) {
+    } else if (vm.contains(DIFF)) {
+      vcs.ShowFileDifference(vm[DIFF].as<std::string>());
+    } else if (vm.contains(REPOSITORIES)) {
       vcs.ShowRepositories();
-    } else if (vm.contains("log")) {
+    } else if (vm.contains(LOG)) {
       vcs.CommitsLog();
-    } else if (vm.contains("commit")) {
-      vcs.DoCommit(vm["commit"].as<std::string>());
-    } else if (vm.contains("branch")) {
-      vcs.CreateBranch(vm["branch"].as<std::string>());
-    } else if (vm.contains("push")) {
+    } else if (vm.contains(COMMIT)) {
+      vcs.DoCommit(vm[COMMIT].as<std::string>());
+    } else if (vm.contains(BRANCH)) {
+      vcs.CreateBranch(vm[BRANCH].as<std::string>());
+    } else if (vm.contains(PUSH)) {
       vcs.Push();
-    } else if (vm.contains("restore")) {
-      vcs.RestoreFiles(vm["restore"].as<size_t>());
+    } else if (vm.contains(RESTORE)) {
+      vcs.RestoreFiles(vm[RESTORE].as<size_t>());
     }
   } catch (po::unknown_option& unknownOption) {
     logging::Log(LOG_EMERG, unknownOption.what());
