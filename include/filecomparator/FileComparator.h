1d044a5db19f8ce180ee0312d6bd1c1c72f40053
Vladimir Kirillov <vkirillov505@gmail.com>
2023-08-27 01:12:21 +0300

    Merge function added

--- include/filecomparator/FileComparator.h
+++ include/filecomparator/FileComparator.h
@@ -9,6 +9,10 @@
 class FileComparator {
  public:
   static void Compare(const std::filesystem::path& p1, const std::filesystem::path& p2);
+
+  static void Merge(const std::filesystem::path& file1,
+                    const std::filesystem::path& file2,
+                    const std::filesystem::path& file3);
 };
 
 #endif  // CODEHUB_FILECOMPARATOR_H
--- src/filecomparator/FileComparator.cpp
+++ src/filecomparator/FileComparator.cpp
@@ -1,18 +1,20 @@
 #include "filecomparator/FileComparator.h"
 
 #include <fstream>
+#include <functional>
 
 #include "dtl/dtl.hpp"
 
 void FileComparator::Compare(const std::filesystem::path& p1,
                              const std::filesystem::path& p2) {
-  std::ifstream f1(p1, std::ios_base::binary);
-  std::ifstream f2(p2, std::ios_base::binary);
   const auto checkOpened = [](const char* path, const bool opened) {
     if (!opened) {
       throw std::runtime_error(fmt::format("File '{}' cannot be opened", path));
     }
   };
+
+  std::ifstream f1(p1, std::ios_base::binary);
+  std::ifstream f2(p2, std::ios_base::binary);
 
   checkOpened(p1.c_str(), f1.is_open());
   checkOpened(p2.c_str(), f2.is_open());
@@ -43,4 +45,34 @@ void FileComparator::Compare(const std::filesystem::path& p1,
 
   diff.composeUnifiedHunks();
   diff.printUnifiedFormat();
+}
+
+void FileComparator::Merge(const std::filesystem::path& file1,
+                           const std::filesystem::path& file2,
+                           const std::filesystem::path& file3) {
+  const auto readContent = [](std::string_view filename) {
+    std::ifstream ifs(filename.data());
+    if (!ifs) {
+      throw std::runtime_error(fmt::format("File '{}' cannot be opened", filename));
+    }
+    return std::string{std::istreambuf_iterator<char>(ifs),
+                       std::istreambuf_iterator<char>()};
+  };
+
+  const auto newContent1 = readContent(file1.string());
+  const auto newContent2 = readContent(file2.string());
+  const auto newContent3 = readContent(file3.string());
+
+  using Element = char;
+  using Sequence = std::string;
+
+  dtl::Diff3<Element, Sequence> diff3(newContent3, newContent1, newContent2);
+  diff3.compose();
+  if (!diff3.merge()) {
+    throw std::runtime_error(
+        fmt::format("Cannot merge files '{}', '{}' and '{}' because of conflict",
+                    file1.c_str(), file2.c_str(), file3.c_str()));
+  }
+
+  std::cout << "Merging result:\n" << diff3.getMergedSequence() << std::endl;
 }
